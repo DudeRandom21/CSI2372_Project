5c3ea4924799a2ea94b13c57da6035bf8e24c5d0
Jonathan <DudeRandom21@gmail.com>
2017-10-28 14:06:42 -0400

    added classes to the header up to QwintoRow

--- scoresheet.h
+++ scoresheet.h
@@ -10,14 +10,29 @@ struct Dice
 	ScoreSheet::Color color;
 	int face;
 	int roll();
-	//** Overload insertion opperator
-		//Not sure if that should to be in the header?
+	ostream &operator<<(ostream &out, const Dice);
 };
 
 struct RandomDice
 {
 	std::uniform_int_distribution die(1, 6);
 	//** I have no idea what goes in here.
+};
+
+struct RollOfDice
+{
+	std::vector<Dice> v; // Instead of storing the Vector internally this should probably inherit form vector - check with prof
+	int roll();
+	RollOfDice pair();
+	operator int();
+	ostream &operator<<(ostream &out, const Dice);
+};
+
+struct QwintoRow<Color>
+{
+	std::array<int, 10> row; // again I this should probably be inherited
+	int &operator[](int index);
+	bool validate(int index, RollOfDice roll);
 };
 
 class ScoreSheet
