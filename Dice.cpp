790de1af57956e11a7b9697ce6e370429530ff4d
Jonathan <DudeRandom21@gmail.com>
2017-11-13 13:25:40 -0500

    implemented all classes in Dice.h into Dice.cpp

---
+++ Dice.cpp
@@ -0,0 +1,56 @@
+#include "Dice.h"
+
+std::uniform_int_distribution<int> RandomDice::die{1,6};
+std::default_random_engine RandomDice::generator{};
+
+Dice::Dice(Color _color) : d_color(_color)
+{
+	roll();
+}
+
+int Dice::roll()
+{
+	return d_face = RandomDice::die(RandomDice::generator);
+}
+
+std::ostream& operator<<(std::ostream& _out, const Dice _dice)
+{
+	_out << _dice.d_face;
+	return _out;
+}
+
+void RollOfDice::roll()
+{
+	for(auto &die : *this)
+	{
+		die.roll();
+	}
+
+}
+
+//revise if inheritance is not permited
+RollOfDice RollOfDice::pair(int _index1, int _index2)
+{
+	RollOfDice pairedRoll;
+	pairedRoll.push_back((*this)[_index1]);
+	pairedRoll.push_back((*this)[_index2]);
+
+	return pairedRoll;
+}
+
+RollOfDice::operator int()
+{
+	int total = 0;
+	for(auto die : *this)
+		total += die.d_face;
+	return total;
+}
+
+std::ostream& operator<<(std::ostream& _out, const RollOfDice _roll)
+{
+	_out << "Roll: ";
+	for(auto die : _roll)
+		_out << die << ", ";
+	
+	_out << std::endl;
+}
--- Dice.h
+++ Dice.h
@@ -7,25 +7,27 @@
 
 enum Color {RED, YELLOW, GREEN, BLUE, WHITE};
 
+struct RandomDice
+{
+	static std::uniform_int_distribution<int> die;
+	static std::default_random_engine generator;
+};
+
 struct Dice
 {
-	Color d_color;
+	Dice(Color _color);
+	const Color d_color;
 	int d_face;
 	int roll();
 	friend std::ostream& operator<<(std::ostream& _out, const Dice _dice);
 };
 
-struct RandomDice
+// should still check with prof if we are allowed to inherit here but in the meantime it's will behave as needed
+struct RollOfDice : public std::vector<Dice>
 {
-	std::uniform_int_distribution<> d_die;
-	//** I have no idea what goes in here.
-};
-
-struct RollOfDice
-{
-	std::vector<Dice> d_v; // Instead of storing the Vector internally this should probably inherit form vector - check with prof
-	int roll();
-	RollOfDice pair();
+	// std::vector<Dice> d_v;
+	void roll();
+	RollOfDice pair(int _index1, int _index2);
 	operator int();
 	friend std::ostream& operator<<(std::ostream& _out, const RollOfDice _roll);
 };
